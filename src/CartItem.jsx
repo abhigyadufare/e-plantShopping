a710f486183f99ffba1870f30338e878f05bfaa2
abhigya <abhigyadufare@gmail.com>
2025-02-18 15:39:52 -0500

    update checkout

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -88,7 +88,7 @@ const CartItem = ({ onContinueShopping, updateAddedToCart, updateTotalQuantity, 
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
       </div>
     </div>
   );
